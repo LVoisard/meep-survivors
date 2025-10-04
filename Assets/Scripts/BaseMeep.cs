3ab791b6edf26ab0f4b74de6e0cff95001d09725
LVoisard <laurentvoisard1@gmail.com>
2025-10-04 12:24:20 -0400

    working on meep

--- Assets/Scripts/BaseMeep.cs
+++ Assets/Scripts/BaseMeep.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class BaseMeep : MonoBehaviour
 {
+    private
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public enum MeepType
+    {
+        Melee,
+        Ranged,
+        Sorcerer
     }
 }
